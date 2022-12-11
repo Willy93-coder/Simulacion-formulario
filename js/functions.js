46cbf909f727caf1a3cdf56005bc7bbbd947eb1a
Willy <guillecg13@gmail.com>
2022-12-11 21:03:56 +0100

    Validación del fomulario sin el input password con javaScript

--- js/functions.js
+++ js/functions.js
@@ -6,8 +6,31 @@ export const validateInput = (e) => {
     validateRegex(regex[inputName], inputValue, inputName);
 };
 
+// Validación del formulario
 export const validateForm = (e) => {
-    e.preventDefault();
+    const inputsLength = inputLength();
+    const usernameLength = inputsLength.username;
+    const firstnameLength = inputsLength.firstname;
+    const emailLength = inputsLength.email;
+    const form = document.getElementById('form');
+
+    if (!validateAllInputsLength(usernameLength, firstnameLength, emailLength)) {
+        e.preventDefault();
+        console.log("No se puede enviar el formulario con errores");
+    } else {
+        e.preventDefault();
+        form.reset();
+        console.log("El formulario se ha enviado correctamente");
+    }
+};
+
+// Función que me devuelve el length de los inputs
+const inputLength = () => {
+    return {
+        username: document.getElementById("username").value.length,
+        firstname: document.getElementById("firstname").value.length,
+        email: document.getElementById("email").value.length,
+    };
 };
 
 // Función que me devuelve un objeto con las expresiones regulares
@@ -17,6 +40,26 @@ const regexExpression = () => {
         firstname: /^[a-zA-ZÀ-ÿ\s]{3,40}$/,
         email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
     };
+};
+
+// Función que valida el length de todos los inputs
+const validateAllInputsLength = (
+    usernameLength,
+    firstnameLength,
+    emailLength
+) => {
+    const usernameValidation = usernameLength >= 4 && usernameLength <= 16;
+    const firstnameValidation = firstnameLength >= 3 && firstnameLength <= 40;
+    const emailValidation = emailLength > 0;
+
+    if (usernameValidation && firstnameValidation && emailValidation) {
+        document.getElementById('success_message').classList.remove("hidden");
+        document.getElementById('error_message').classList.add("hidden");
+        return true;
+    } else {
+        document.getElementById('error_message').classList.remove("hidden");
+        return false;
+    }
 };
 
 // Validar los inputs con las expresiones regulares
